a7d04bd8769dcbb7d639b7452241f0d40abc981c
amit4alljava <amit4alljava@gmail.com>
2017-09-06 16:38:17 +0530

    Codes

---
+++ Codes6/js/itemoperations.js
@@ -0,0 +1,32 @@
+// CRUD 
+const itemOperations = {
+itemList:[],
+id:1,	
+addItem(name,descr, itemdate, branch , color){
+	var itemObject = new Item(this.id,name,descr, itemdate, branch , color);
+	this.itemList.push(itemObject);
+	this.id++;
+},
+deleteItem(){
+	 return this.itemList = this.itemList.filter(function(itemObject){
+		return itemObject.markForDelete ==false;
+});
+	
+},
+searchById(id){
+	var index = this.itemList.findIndex(function(itemObject){
+		return itemObject.id ==id;
+	});
+	return index;
+},
+countMark(){
+	return this.itemList.filter(function(itemObject){
+		return itemObject.markForDelete ;
+	}).length;
+},	
+markRecordDelete(id){
+	var index = this.searchById(id);
+	var itemObject = this.itemList[index];
+	itemObject.toggleDelete();
+}	
+}
